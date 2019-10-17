4e4dfef173170443fc7ba875877b1e152a7ae4d0
Moumen.Amin <Moumen.Amin@MOUMEN-AMIN>
2019-10-17 12:56:15 +0200

    adding reverseInt problem

--- Program.cs
+++ Program.cs
@@ -7,24 +7,21 @@ using System.Threading.Tasks;
 using AlgoCastsCs.completedExcersices.Maxchar;
 using AlgoCastsCs.completedExcersices.Palindromes;
 using AlgoCastsCs.completedExcersices.reverseString;
+using AlgoCastsCs.completedExcersices.ReversingInt;
 
 namespace AlgoCastsCs
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            ReverseString.ReverseStringProblem("abcd");
-            ReverseString.ReverseStringProblem2("abcd");
+        {           
             ReverseString.Test();
-            /////////////////////////////////////// 
-            Palindrome.PalindromeProblem("ab  ba");
-            Palindrome.PalindromeProblem("abcdefg");
+            ///////////////////////////////////////            
             Palindrome.Test();
-            ///////////////////////////////////////
-            MaxChar.MaxCharProblem("abcdefghijklmnaaaaa");
-            MaxChar.MaxCharProblem("ab1c1d1e1f1g1");
+            ///////////////////////////////////////           
             Palindrome.Test();
+            ///////////////////////////////////////            
+            ReverseInt.Test();
         }
 
     }
---
+++ completedExcersices/ReversingInt/ReverseInt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoCastsCs.completedExcersices.ReversingInt
+{
+    // --- Directions
+    // Given an integer, return an integer that is the reverse
+    // ordering of numbers.
+    // --- Examples
+    //   reverseInt(15) === 51
+    //   reverseInt(981) === 189
+    //   reverseInt(500) === 5
+    //   reverseInt(-15) === -51
+    //   reverseInt(-90) === -9
+    public static class ReverseInt
+    {
+        public static int ReverseIntProblem(int n)
+        {
+            var numberSign = Math.Sign(n);
+            var numberString = Math.Abs(n).ToString();
+            var reversedNumberString = new string(numberString.Reverse().ToArray());
+            var result = Convert.ToInt32(reversedNumberString) * numberSign;
+            return result;
+        }
+
+        public static void Test()
+        {
+            Debug.Assert(ReverseIntProblem(0) == 0);
+            Debug.Assert(ReverseIntProblem(5) == 5);
+            Debug.Assert(ReverseIntProblem(15) == 51);
+            Debug.Assert(ReverseIntProblem(90) == 9);
+            Debug.Assert(ReverseIntProblem(2359) == 9532);
+            Debug.Assert(ReverseIntProblem(-5) == -5);
+            Debug.Assert(ReverseIntProblem(-15) == -51);
+            Debug.Assert(ReverseIntProblem(-90) == -9);
+            Debug.Assert(ReverseIntProblem(-2359) == -9532);
+        }
+    }
+}
