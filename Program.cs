b299943a83b91557ded8476a2ffd61678ffb49b9
Moumen.Amin <Moumen.Amin@MOUMEN-AMIN>
2019-10-17 12:31:22 +0200

    adding max char problem

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AlgoCastsCs.completedExcersices.Maxchar;
 using AlgoCastsCs.completedExcersices.Palindromes;
 using AlgoCastsCs.completedExcersices.reverseString;
 
@@ -13,13 +14,18 @@ namespace AlgoCastsCs
     {
         static void Main(string[] args)
         {
-            ReverseString.ReverseStringProblem("abcd");            
+            ReverseString.ReverseStringProblem("abcd");
             ReverseString.ReverseStringProblem2("abcd");
             ReverseString.Test();
             /////////////////////////////////////// 
             Palindrome.PalindromeProblem("ab  ba");
             Palindrome.PalindromeProblem("abcdefg");
             Palindrome.Test();
+            ///////////////////////////////////////
+            MaxChar.MaxCharProblem("abcdefghijklmnaaaaa");
+            MaxChar.MaxCharProblem("ab1c1d1e1f1g1");
+            Palindrome.Test();
         }
+
     }
 }
---
+++ completedExcersices/Maxchar/MaxChar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoCastsCs.completedExcersices.Maxchar
+{
+
+    // --- Directions
+    // Given a string, return the character that is most
+    // commonly used in the string.
+    // --- Examples
+    // maxChar("abcccccccd") === "c"
+    // maxChar("apple 1231111") === "1"
+    public static class MaxChar
+    {
+        public static char MaxCharProblem(string str)
+        {
+            var result = char.MinValue;
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            foreach (var character in str)
+            {
+                if (dictionary.ContainsKey(character))
+                    dictionary[character]++;
+                else
+                    dictionary.Add(character, 1);
+            }
+
+            int maxCount = 0;
+            foreach (var charCount in dictionary)
+            {
+                if (charCount.Key > maxCount)
+                    result = charCount.Key;
+            }
+            return result;
+        }
+
+        public static void Test()
+        {
+            Debug.Assert(MaxCharProblem("abcdefghijklmnaaaaa") == 'a');
+            Debug.Assert(MaxCharProblem("ab1c1d1e1f1g1") == '1');            
+        }
+    }
+}
